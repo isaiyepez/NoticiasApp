69f0a690e9d4256ffe95f95ecb9a85c43626e7eb
iyepez <isaiyepez@gmail.com>
2019-04-23 23:13:16 -0500

    Functionality to post new news

--- src/app/news-add/news-add.page.ts
+++ src/app/news-add/news-add.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Autor } from '../models/autor.models';
+import { NoticiasService } from '../services/noticias.service';
+import { Noticia } from '../models/noticia.models';
+import { LoadingController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-news-add',
@@ -7,9 +11,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewsAddPage implements OnInit {
 
-  constructor() { }
+  autores: Autor[] = new Array<Autor>();
+  noticia: Noticia = new Noticia();
+  constructor(private noticiaServicio: NoticiasService, public toastController: ToastController, public loadingController: LoadingController) {}
 
   ngOnInit() {
+    this.noticiaServicio.verAutores().subscribe((listadoAutores)=>{
+      this.autores = listadoAutores
+    })
+  }
+
+  async guardar()
+  {
+    const loading = await this.loadingController.create({
+      message: 'Saving...',
+    });
+    await loading.present();
+    
+    this.noticiaServicio.agregarNoticia(this.noticia).subscribe(()=>{
+      loading.dismiss();
+      this.mostrarMensaje("New Saved");
+    },
+    error=>{
+      console.log("IYG: Error" + error);
+      this.mostrarMensaje(error);
+      loading.dismiss();
+    });
+  }
+
+  async mostrarMensaje(mensaje: string)
+  {
+    const toast = await this.toastController.create({
+      message: mensaje,
+      duration: 2000
+    });
+    toast.present();
   }
 
 }
--- src/app/services/noticias.service.ts
+++ src/app/services/noticias.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Noticia } from '../models/noticia.models';
+import { Autor } from '../models/autor.models';
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +17,14 @@ export class NoticiasService {
 
   eliminaNoticia(noticiaID : number) : Observable<Boolean> {
     return this.http.get<Boolean>("https://localhost:44343/api/Noticias/Eliminar/" + noticiaID)
+  }
+
+  agregarNoticia(noticia: Noticia) : Observable<Boolean> {
+    return this.http.post<Boolean>("https://localhost:44343/api/Noticias/Agregar", noticia)
+  } 
+
+  verAutores(): Observable<Autor[]> {
+    return this.http.get<Autor[]>("https://localhost:44343/api/Noticias/VerAutores")
   }
 
 }
